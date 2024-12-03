2e9dcf900e9a2a83b6797f3a72efbf3d36882053
d0m-1k <d0m_1k@mail.ru>
2024-12-03 18:26:40 +0500

    main

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using nnn;
+
+namespace nnn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nnn_sevice nnns = new nnn_sevice();
+            nnns.startProcess("taskmgr.exe");
+        }
+    }
+}
---
+++ ConsoleApp4/nnn-sevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace nnn
+{
+    internal class nnn_sevice
+    {
+        public bool startProcess(string applicationPath, string arguments = "", bool createWindow = true)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = applicationPath,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    CreateNoWindow = !createWindow
+                };
+
+                using (Process process = Process.Start(startInfo)) { process.WaitForExit(); }
+                return true;
+            }
+            catch (Exception ex) { return false; }
+        }
+    }
+}
